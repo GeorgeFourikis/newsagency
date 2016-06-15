3e96348e9395d06c85dfb00bd193cf962df76316
GeorgeFourikis <you@example.cogit config --global user.email you@example.co>
2016-06-15 10:26:23 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Newsagency::Application.config.secret_key_base = '9a2918c901906d5f7a276bfe2054dc84a087d378f56c73a71a4e178505fe8c03ee1da1feebad7ac95d831760cbe45a71cc591853e1d5640f1fef82a0ea5055c5'
